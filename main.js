85ca6b1f1f047384f105f14e524639d1273da622
Jooksta <teslachipset@gmail.com>
2018-09-28 18:18:29 +0200

    ajout ficier main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+setTimeout (function() {alert("Hey le site xxx Hentai.du.Turfu xxx est trop bien. Viens dessus stp please");
+if (confirm("Clique sur Ok pour aller sur xxx Hentai.du.Turfu xxx ;)"))
+location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+ }, 10000);
